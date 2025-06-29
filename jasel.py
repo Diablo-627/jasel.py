93ea8c3953f588c744023d333e9d3621e006735d
Diablo-627 <150846607+Diablo-627@users.noreply.github.com>
2025-06-29 17:42:31 +0500

    Update jasel.py

--- jasel.py
+++ jasel.py
@@ -129,4 +129,6 @@ def map_view():
     return render_template_string(html_template, points=points)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
